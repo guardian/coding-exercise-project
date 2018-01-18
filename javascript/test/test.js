165fc375a85d83943ab554dde1b1bd73e789e2e7
Michael Barton <michael.barton@guardian.co.uk>
2018-01-18 10:28:40 +0000

    Switch to Jest for tests

--- javascript/test/test.js
+++ javascript/test/test.js
@@ -1,12 +1,3 @@
-
-var assert = require('assert')
-
-var gol = require('../src/game-of-life')
-
-describe('Game of Life', function() {
-    describe('a test', function() {
-        it('should be failing', function() {
-            assert(gol.value, 'Change this to be more useful');
-        });
-    });
+test('a failing test', () => {
+    expect(false).toBe(true);
 });
