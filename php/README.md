2736570c683f62347cf199ae5f73bc1f1bbccbff
Michael Barton <michael.barton@guardian.co.uk>
2019-02-11 14:14:33 +0000

    PHP skeleton

---
+++ php/README.md
@@ -0,0 +1,6 @@
+# PHP Pairing Test
+
+- `brew install php`
+- `brew install composer`
+- Write code in `code.php`. Run with `composer run`.
+- Write PHPUnit `tests.php`. Run with `composer test`.
---
+++ php/tests.php
@@ -0,0 +1,12 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class PairingTest extends TestCase
+{
+    public function testSkeleton()
+    {
+        $this->assertSame(true, false);
+    }
+}
+
+?>
