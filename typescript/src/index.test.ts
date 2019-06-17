b692a5671a7216c672dea7f85abc2a005478f963
RichieAHB <richard.beddington@guardian.co.uk>
2019-06-17 14:22:06 +0100

    Remove redundant types and add test scaffold

---
+++ typescript/src/index.test.ts
@@ -0,0 +1,7 @@
+import { myConst } from ".";
+
+describe("Pairing test", () => {
+  it("runs", () => {
+    expect(myConst).toBe(true);
+  });
+});
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,1 +1,3 @@
-console.log("Hello World");
+const myConst = true;
+
+export { myConst };
