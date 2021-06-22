a49ea5c7cee8f279e38e25e2dbf0c26d98aecb8c
Max Duval <max.duval@theguardian.com>
2021-06-22 14:55:37 +0100

    update scalatest to work with scala 2.13 (#64)

--- scala/src/test/scala/gu/com/MainTest.scala
+++ scala/src/test/scala/gu/com/MainTest.scala
@@ -1,8 +1,9 @@
 package gu.com
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.flatspec.{AnyFlatSpec}
+import org.scalatest.matchers.should.{Matchers}
 
-class MainTest extends FlatSpec with Matchers {
+class MainTest extends AnyFlatSpec with Matchers {
   "whereWeLive" should "be Earth" in {
     Main.whereWeLive should be ("Earth")
   }
