13d7fe79c310d0057cb4cb47f4b28929c4ddc39e
Michael Clapham <michaelclapham@users.noreply.github.com>
2026-03-18 17:43:09 +0000

    Migrate test from XCTest to Swift testing

--- swift/Tests/coding_exercise_gu_tests/coding_exercise_tests.swift
+++ swift/Tests/coding_exercise_gu_tests/coding_exercise_tests.swift
@@ -1,10 +1,15 @@
-import XCTest
-@testable import coding_exercise_gu // Allows access to internal members
+import Testing
+@testable import coding_exercise_gu
+
+// Documentation on Swift Testing
+// https://developer.apple.com/xcode/swift-testing/
+// https://developer.apple.com/documentation/testing/definingtests
 
 
-final class MainTests: XCTestCase {
+final class MainTests {
+    @Test("function should return 10")
     func testExample() {
         let value = coding_exercise_gu.someNumber()
-        XCTAssertEqual(value, 10, "Value should be 10")
+        #expect(value == 10, "The someNumber function should return 10")
     }
 }
